54cc5ee4d9610c0c8244f5245c3eca6a425d2884
ravichaurasia2021 <ravi.chaurasia1118@gmail.com>
2021-07-11 01:40:43 +0530

    code update

--- app/modules/booking/controllers/booking.controller.js
+++ app/modules/booking/controllers/booking.controller.js
@@ -9,7 +9,12 @@ const User = require('../../user/models/user.model');
 // access   Public
 exports.eventBooking = asyncHandler(async (req, res, next) => {
     req.body.userId = req.user._id;
-    req.body.bookingDate = new Date(Date.now())
+    req.body.bookingDate = new Date(Date.now());
+    let find1 = await Booking.find({userId : req.user._id,eventId: req.body.eventId });
+    if(find1){
+        return next(new ErrorResponse("You already booked this event", 401));
+
+    }
     const booking = await Booking.create(req.body);
 
     res.status(200).json({
